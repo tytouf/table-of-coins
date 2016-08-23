6cecc7e0f142979dc1190378b8a453fc7d37e6a2
Christophe Augier <christophe.augier@gmail.com>
2016-08-23 20:12:59 +0200

    Initial commit of tool
    First commit with python program to generate SVG files
    Examples images and simple readme file.

---
+++ README.md
@@ -0,0 +1,13 @@
+==== Table of Coins
+
+A simple python program used to make a table covered with coins.
+The program generates an SVG file with staggered (or not) rows of coins
+with the option to use a black and white image to mask the coins.
+
+A picture being better than words: a map of Europe and the rendered SVG
+file.
+
+![alt text](https://github.com/tytouf/table-of-coins/raw/master/images/europe.png "Map of Europe")
+
+![alt text](https://github.com/tytouf/table-of-coins/raw/master/images/europe.svg "Rendered map of Europe")
+
---
+++ gen_svg.py
@@ -0,0 +1,44 @@
+from optparse import OptionParser
+from PIL import Image
+import math
+
+def my_range(start, stop, step):
+    r = start
+    while r < stop:
+        yield r
+        r += step
+
+
+parser = OptionParser()
+parser.add_option("-i", "--image", dest="image", type="string",
+                  help="Image used to mask coins")
+parser.add_option("--width", dest="width", type="int",
+                  default=1200, help="Output width")
+parser.add_option("--height", dest="height", type="int",
+                  default=700, help="Output height")
+parser.add_option("-d", "--diameter", dest="diameter", type="float",
+                  default=16.5, help="Coin diameter")
+parser.add_option("-s", "--staggered", dest="staggered",
+                  action="store_true", help="Staggered rows")
+
+(options, args) = parser.parse_args()
+
+if options.image:
+    img = Image.open(options.image)
+    #img.resize(options.width, options.height)
+    data = list(img.getdata(0)) # get Red color
+
+print('<svg width="{}mm" height="{}mm">'.format(options.width, options.height))
+
+radius = options.diameter / 2
+y_step = options.diameter if not options.staggered else options.diameter * math.cos(30*math.pi/180)
+x_offset = 0
+
+for y in my_range(radius, options.height, y_step):
+    for x in my_range(radius - x_offset, options.width, options.diameter):
+        i = math.floor(y) * options.width + math.floor(x)
+        if data[i] < 120:
+            print ('<circle cx="{}mm" cy="{}mm" r="{}mm"></circle>'.format(x, y, radius))
+    x_offset = radius if x_offset == 0 else 0
+
+print('</svg>')
